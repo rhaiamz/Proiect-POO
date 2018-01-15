97d18d510ba931b1df49e9aa4395d7b9af6774c0
Mihael03 <35213171+Mihael03@users.noreply.github.com>
2018-01-15 08:29:03 +0200

    Update PossibleChar.cpp

--- PossibleChar.cpp
+++ PossibleChar.cpp
@@ -9,7 +9,9 @@ PossibleChar::PossibleChar(std::vector<cv::Point> _contour)
 
     boundingRect = cv::boundingRect(contour);
 
-   
+    intCenterX = (boundingRect.x + boundingRect.x + boundingRect.width) / 2;
+    intCenterY = (boundingRect.y + boundingRect.y + boundingRect.height) / 2;
+    
     dblDiagonalSize = sqrt(pow(boundingRect.width, 2) + pow(boundingRect.height, 2));
 
     dblAspectRatio = (float)boundingRect.width / (float)boundingRect.height;
